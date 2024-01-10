05efca2011590aaaa0ec5c45946fc7fa2c677c95
vladg <vlad.grush.99@gmail.com>
2024-01-10 15:09:37 +0200

    apply migrations when app is starting

--- EntityFramework_practice/Extension/MigrationProvider.cs
+++ EntityFramework_practice/Extension/MigrationProvider.cs
@@ -10,20 +10,52 @@ public static class MigrationProvider
     {
         using var scope = application.Services.CreateScope();
         InitAnnotationDb(scope);
+        InitPropertyDb(scope);
+        InitFluentApiDb(scope);
     }
 
     private static void InitAnnotationDb(IServiceScope scope)
     {
-        var migrationFiles = Directory.GetFiles(Environment.CurrentDirectory + "\\Migration\\DataAnnotation");
         var annotationContext = scope.ServiceProvider.GetRequiredService<DbContextApp>();
-        if (migrationFiles.Any())
+        if (!annotationContext.Database.EnsureCreated())
         {
-            for (int i = 0; i < migrationFiles.Length - 1; i += 2)
+            var migrationNames = GetMigrationNames("\\Migration\\DataAnnotation");
+            foreach (var migrationName in migrationNames)
             {
-                var migrationName = migrationFiles[i].Split("\\")[^1]
-                    .Split(".")[0];
-               annotationContext.GetInfrastructure().GetService<IMigrator>()!.MigrateAsync(migrationName);
-            }
+                annotationContext.GetInfrastructure().GetService<IMigrator>()!.MigrateAsync(migrationName);
+            }   
+        }
+    }
+    
+    private static void InitPropertyDb(IServiceScope scope)
+    {
+        var annotationContext = scope.ServiceProvider.GetRequiredService<DbContextApp>();
+        var migrationNames = GetMigrationNames("\\Migration\\DataProperty");
+        foreach (var migrationName in migrationNames)
+        {
+            annotationContext.GetInfrastructure().GetService<IMigrator>()!.MigrateAsync(migrationName);
+        }
+    }
+    
+    private static void InitFluentApiDb(IServiceScope scope)
+    {
+        var annotationContext = scope.ServiceProvider.GetRequiredService<DbContextApp>();
+        var migrationNames = GetMigrationNames("\\Migration\\FluentApi");
+        foreach (var migrationName in migrationNames)
+        {
+            annotationContext.GetInfrastructure().GetService<IMigrator>()!.MigrateAsync(migrationName);
+        }
+    }
+
+    private static IEnumerable<string> GetMigrationNames(string directoryPath)
+    {
+        var migrationFiles = Directory.GetFiles(Environment.CurrentDirectory + directoryPath);
+        for (int i = 0; i < migrationFiles.Length - 1; i += 2)
+        {
+            var migrationName = migrationFiles[i].Split("\\")[^1]
+                .Split(".")[0];
+
+            yield return migrationName;
         }
     }
 }
